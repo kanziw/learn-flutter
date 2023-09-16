658fe5643de8758cb42903441c0fd5242565f949
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-16 22:08:50 +0900

    Add Counter using StatefulWidget

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,6 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/stateless_widget_app.dart';
 
 void main() {
-  runApp(const StatelessWidgetApp());
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void onClicked() {
+    setState(() {
+      counter += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Click Count',
+                style: TextStyle(fontSize: 30),
+              ),
+              Text(
+                '$counter',
+                style: const TextStyle(fontSize: 30),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked,
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
