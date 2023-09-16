77a957a77c88fed86064190904cb344dd84b9398
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-17 00:35:20 +0900

    Add Image

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
+// @see https://gist.github.com/preinpost/941efd33dff90d9f8c7a208da40c18a9
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
+
   runApp(const App());
 }
 
