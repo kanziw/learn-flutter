4fc66618c94d97694215b7f11a60a89f1b467f5a
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-17 00:01:10 +0900

    Fetch in HomeScreen

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    try {
+      webtoons = await ApiService.getTodaysToons();
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('$webtoons, $isLoading');
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
