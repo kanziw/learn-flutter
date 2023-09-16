d82b4c9b19107906e90d9dce58b29cdabd19899f
Jiwoong Jung <kanziwoong@gmail.com>
2023-09-16 23:24:31 +0900

    Separate pomodoro app

---
+++ toonflix/lib/pomodoro_app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/screens/pomodoro_screen.dart';
+
+class PomodoroApp extends StatelessWidget {
+  const PomodoroApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          headlineLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
